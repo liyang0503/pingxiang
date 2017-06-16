4e169aab480eeb0f9b0bd15ca220356acf23a803
liyang0503 <liyang0503@126.com>
2017-06-16 10:13:16 +0800

    pingxing

--- js/index.js
+++ js/index.js
@@ -7,4 +7,12 @@ $(function () {
 //   tab切换调用
    tab('.rt-li', '.rt-ul');
    tab('.rt-li2', '.rt-ul2');
+
+//   右侧切换部分的宽度（手机）
+   if (w < 767) {
+      $('.rt-new-top').each(function () {
+         var rt_top = this;
+         $(rt_top).find('div').css({width: $(rt_top).outerWidth() - $(rt_top).find('span').outerWidth() - 8});
+      });
+   }
 });
